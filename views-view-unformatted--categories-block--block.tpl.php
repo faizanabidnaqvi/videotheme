d56b1b4c03117c89312d837414f83e71ead5a1e4
faizanabidnaqvi <faizan.abid@gmail.com>
2014-02-01 22:31:08 +0800

    Modified categories views block to be single rows

--- views-view-unformatted--categories-block--block.tpl.php
+++ views-view-unformatted--categories-block--block.tpl.php
@@ -1,14 +1,8 @@
 <?php 
-$n1 = $n2 = '';
-$i = true;
+$n1 = '';
+
 foreach ($rows as $id => $row) {
-  if ($i) {
     $n1 .= '<li>'.$row.'</li>';
-    $i = false;
-  }else{
-    $n2 .= '<li>'.$row.'</li>';
-    $i = true;
-  }
 }
-print '<div class="row"><div class="span2"><ul>'.$n1.'</ul></div><div class="span2"><ul>'.$n2.'</ul></div></div>';
+print '<div class="row"><div class="span2"><ul>'.$n1.'</ul></div>';
 ?>
